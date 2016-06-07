69c05e58bc9ff43bd06cb68752e855b3037f5f95
Vadim Merkotan <Persidskij_on_ferrari@ukr.net>
2016-06-07 06:49:38 +0300

    commit from parent

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,30 +1,66 @@
 import React, { PropTypes, Component } from 'react'
+import { findDOMNode } from 'react-dom'
 import Article from './Article'
-import ArticleListOpenedArticle from '../decorators/ArticleListOpenedArticle'
+import Chart from './Chart'
+import oneOpen from '../decorators/oneOpen'
+import Select from 'react-select'
+
+import 'react-select/dist/react-select.css'
 
 class ArticleList extends Component {
 
+    state = {
+        selected: null
+    }
+
+    componentDidMount() {
+        console.log('---', 2)
+        console.log('---', findDOMNode(this.refs.chart))
+    }
+
     render() {
-        const { articles, toggleOpen, openedArticle } = this.props
+        const { articles, isOpen, openItem } = this.props
 
         const articleItems = articles.map((article) => <li key={article.id}>
             <Article article = {article}
-                     isOpen = {article.id === openedArticle}
-                openArticle = {toggleOpen(article.id)}
+                     isOpen = {isOpen(article.id)}
+                openArticle = {openItem(article.id)}
             />
         </li>)
 
+        const options = articles.map((article) => ({
+            label: article.title,
+            value: article.id
+        }))
+
         return (
-            <ul>
-                {articleItems}
-            </ul>
+            <div>
+                <ul>
+                    {articleItems}
+                </ul>
+                <Chart ref="chart" />
+                <Select
+                    options = {options}
+                    onChange = {this.handleChange}
+                    value= {this.state.selected}
+                    multi = {true}
+                />
+            </div>
         )
     }
 
+    handleChange = (selected) => {
+        this.setState({
+            selected
+        })
+    }
 }
 
 ArticleList.propTypes = {
-    articles: PropTypes.array.isRequired
+    articles: PropTypes.array.isRequired,
+
+    isOpen: PropTypes.func.isRequired,
+    openItem: PropTypes.func.isRequired
 }
 
-export default ArticleListOpenedArticle(ArticleList)
+export default oneOpen(ArticleList)
